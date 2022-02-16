93e5f0e98370d8b7a55e26a91110f78652eb2a5d
Gabriel Muniz <gabriel.munizz@outlook.com.br>
2022-02-16 12:19:06 -0300

    Requisito 03: Criado a pagina de configuraçoes, adicionado a rota e criado o titulo

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './Pages/Login';
 import Game from './Pages/Game';
 import './App.css';
+import Settings from './Pages/Settings';
 
 export default function App() {
   return (
     <Switch>
+      <Route path="/settings" component={ Settings } />
       <Route path="/game" component={ Game } />
       <Route path="/" component={ Login } />
     </Switch>
