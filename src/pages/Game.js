f8bf6f1264abd81c4a194c8bd05f90ef741e0b50
Andreyrvs <cortaesai@gmail.com>
2022-03-05 11:36:27 -0300

    Requisito 05: Adiciona logica de gerar novo token

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,17 +1,60 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
-import fetchTokenAPI from '../services';
+import tokenThunk from '../redux/actions';
+import fetchTokenAPI, { fetchQuestionsAPI } from '../services';
 
 export class Game extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results: [],
+    };
+  }
+
   componentDidMount() {
-    fetchTokenAPI();
+    this.generateToken();
+  }
+
+  async generateToken() {
+    const { tokenThunks } = this.props;
+
+    const RESPONSE_CODE_THREE = 3;
+    const getlocalToken = localStorage.getItem('token');
+    const questions = await fetchQuestionsAPI(getlocalToken);
+    console.log(questions);
+    if (getlocalToken) {
+      this.setState({
+        results: [questions.results],
+      });
+    }
+    if (questions.response_code === RESPONSE_CODE_THREE) {
+      await tokenThunks();
+      const newToken = localStorage.getItem('token');
+      const newQuestions = await fetchQuestionsAPI(newToken);
+      this.setState({
+        results: [newQuestions.results],
+      });
+    }
   }
 
   render() {
     return (
-      <div><Header /></div>
+      <section>
+        <Header />
+      </section>
     );
   }
 }
 
-export default Game;
+const mapDispatchToProps = (dispatch) => ({
+  tokenThunks: () => dispatch(tokenThunk()),
+});
+
+Game.propTypes = {
+  tokenThunks: PropTypes.func,
+}.isRequire;
+
+export default connect(null, mapDispatchToProps)(Game);
