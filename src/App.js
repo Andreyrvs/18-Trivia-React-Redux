71250c6bba6dd8d1e09ca1337f8d9210e93efb76
Andreyrvs <cortaesai@gmail.com>
2022-03-05 10:13:20 -0300

    REFATORANDO

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Login from './Pages/Login';
-import Game from './Pages/Game';
+import logo from './trivia.png';
 import './App.css';
-import Settings from './Pages/Settings';
 
 export default function App() {
   return (
-    <Switch>
-      <Route path="/settings" component={ Settings } />
-      <Route path="/game" component={ Game } />
-      <Route path="/" component={ Login } />
-    </Switch>
+    <div className="App">
+      <header className="App-header">
+        <img src={ logo } className="App-logo" alt="logo" />
+        <p>
+          SUA VEZ
+        </p>
+      </header>
+    </div>
   );
 }
