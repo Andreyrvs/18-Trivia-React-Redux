53cbb793ac3d1a885eded11551327ab3ff88593a
Andreyrvs <cortaesai@gmail.com>
2022-03-05 19:55:25 -0300

    Inicia req 08

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -6,10 +6,17 @@ class Question extends Component {
   constructor() {
     super();
     this.handleAnswers = this.handleAnswers.bind(this);
+    this.handleBorderColor = this.handleBorderColor.bind(this);
+
+    this.state = {
+      correctColor: '',
+      incorrectColor: '',
+    };
   }
 
   handleAnswers() {
     const { results } = this.props;
+    const { correctColor, incorrectColor } = this.state;
     const NUMBER_FIVE = 0.5;
     if (results.length > 0) {
       const answers = [...results[0].incorrect_answers, results[0].correct_answer];
@@ -20,7 +27,15 @@ class Question extends Component {
           {shuffled.map((item, index) => (
             <section key={ index } data-testid="answer-options">
               <Button
+                style={ { border: `${
+                  results[0].correct_answer === item
+                    ? correctColor
+                    : incorrectColor}`,
+                } }
                 btnType="button"
+                handleClick={ () => this.handleBorderColor(
+                  results[0].correct_answer, item,
+                ) }
                 dataTest={ results[0].correct_answer === item
                   ? 'correct-answer'
                   : `wrong-answer-${index}` }
@@ -35,9 +50,21 @@ class Question extends Component {
     }
   }
 
+  handleBorderColor(correctAnswer, item) {
+    return correctAnswer === item
+      ? this.setState({
+        correctColor: '3px solid rgb(6, 240, 15)',
+        incorrectColor: '3px solid rgb(255, 0, 0)',
+      })
+      : this.setState({
+        incorrectColor: '3px solid rgb(255, 0, 0)',
+        correctColor: '3px solid rgb(6, 240, 15)',
+      });
+  }
+
   render() {
     const { results } = this.props;
-
+    console.log(results);
     if (results.length === 0) {
       return <h1>Loading</h1>;
     }
