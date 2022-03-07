59db6a8ee044608b646ff56d6563c7928327c88d
Gabriel Muniz <gabriel.munizz@outlook.com.br>
2022-03-07 20:29:36 -0300

    Requisito 13: Finalizado

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import '../components/components.css';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 export class Feedback extends Component {
+  handleFeedback = () => {
+    const WELL_DONE_ASSERTIONS = 3;
+    const { assertions } = this.props;
+    console.log(assertions);
+    const isWellDone = assertions >= WELL_DONE_ASSERTIONS;
+    return isWellDone ? (
+      <h1 data-testid="feedback-text">Well Done!</h1>
+    ) : (
+      <h1 data-testid="feedback-text">Could be better...</h1>
+    );
+  }
+
   render() {
     return (
       <div data-testid="feedback-text">
         <Header />
+        {this.handleFeedback()}
       </div>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  assertions: PropTypes.number,
+}.isRequired;
+
+const mapStateToProps = ({ player }) => ({
+  assertions: player.assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);
